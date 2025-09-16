c01c2e61cd074d71361f508901db06339de0b207
hegdedvh <124718860+hegdedvh@users.noreply.github.com>
2025-09-16 23:11:57 +0530

    Time: 2 ms (98.89%) | Memory: 45.3 MB (10.33%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int c=target-nums[i];
+            if (m.containsKey(c)){
+                return new int[] {m.get(c),i};
+            }
+            m.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+        
+        
+    }
+}
